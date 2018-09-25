a7bd4a168b5e6018a5866c3a184d5579bb0620c1
Harrison Smith <hbs@asu.edu>
2018-09-25 14:15:04 -0700

    added retrieve_kegg file

---
+++ retrieve_kegg.py
@@ -0,0 +1,10 @@
+## Gather map, ec, rn, cpd detailed files
+
+## Get list of each of map, ec, rn, cpd ids and names
+
+## Loop through lists to retrieve detailed files
+
+## Add detailed information for reactions
+
+## Retrieve links between all combinations of map, ec, rn, cpd
+
