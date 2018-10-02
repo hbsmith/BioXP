8517db63ad7a602aad9e9d2e755e5e4db7c9bbad
Harrison Smith <hbs@asu.edu>
2018-10-01 17:42:25 -0700

    cleaned up netexp

--- netexp.jl
+++ netexp.jl
@@ -1,55 +1,45 @@
 ## Network expansion using lists/dicts
 import JSON
 
-reaction_edges = JSON.parsefile("../kegg/2018-09-25/reaction_edges.json");
-reactions = [i for i in keys(reaction_edges["products"])] ## could use ["substrates""] too
+function prepare_matrices_and_targets(reaction_edges_json::String,target_json::String)
 
-cpd_reactants_flat = unique(Iterators.flatten(values(reaction_edges["substrates"])))
-cpd_products_flat = unique(Iterators.flatten(values(reaction_edges["products"])))
-compounds = unique(Iterators.flatten([cpd_reactants_flat,cpd_products_flat]));
+    reaction_edges = JSON.parsefile(reaction_edges_json)
 
+    reactions = [i for i in keys(reaction_edges["products"])] ## could use ["substrates""] too
 
-## For R and P:
-## Reactons in columns
-## Compounds in rows
-## Rows, Columns
-## This is equivilent to R and P in segrelab's github
-R = zeros(Int, (length(compounds),length(reactions)))
-P = zeros(Int, (length(compounds),length(reactions)));
+    cpd_reactants_flat = unique(Iterators.flatten(values(reaction_edges["substrates"])))
+    cpd_products_flat = unique(Iterators.flatten(values(reaction_edges["products"])))
+    compounds = unique(Iterators.flatten([cpd_reactants_flat,cpd_products_flat]))
 
-for (i,r) in enumerate(reactions)
-    R[:,i] = [Int(cpd in reaction_edges["substrates"][r]) for cpd in compounds]
-    P[:,i] = [Int(cpd in reaction_edges["products"][r]) for cpd in compounds]
+    ## For R and P:
+    ## Reactons in columns
+    ## Compounds in rows
+    ## Rows, Columns
+    ## This is equivilent to R and P in segrelab's github
+    R = zeros(Int, (length(compounds),length(reactions)))
+    P = zeros(Int, (length(compounds),length(reactions)))
+
+    for (i,r) in enumerate(reactions)
+        R[:,i] = [Int(cpd in reaction_edges["substrates"][r]) for cpd in compounds]
+        P[:,i] = [Int(cpd in reaction_edges["products"][r]) for cpd in compounds]
+    end
+    ################################################################################
+    target_compounds = JSON.parsefile(target_json);
+    target_compounds_org = collect(intersect(keys(target_compounds),compounds))
+    t = [Int(i in target_compounds_org) for i in compounds]
+    ################################################################################
+    (R,P,compounds,reactions,t)
+
 end
 
-# seed_compounds = JSON.parsefile("../seeds.json");
-# x = [Int(i in seed_compounds["Enceladus_20-SAFR-032"]) for i in compounds];
+function prepare_seeds(seed_list::Array{String,1},compounds::Array{String,1})
+    [Int(i in seed_list) for i in compounds]
+end
 
-# ds80_seeds = ["C00011",
-# "C20298",
-# "C14819",
-# "C00087",
-# "C00237",
-# "C00058",
-# "C00033",
-# "C00031",
-# "C00095",
-# "C00124",
-# "C00159",
-# "C00243",
-# "C00208",
-# "C00282",
-# "C00007",
-# "C00001"]
-
-ds80_seeds = ["C00031","C00001"]
-
-x = [Int(i in ds80_seeds) for i in compounds];
-
-
-target_compounds = JSON.parsefile("../targets/Freilich09.json");
-target_compounds_org = collect(intersect(keys(target_compounds),compounds));
-t = [Int(i in target_compounds_org) for i in compounds];
+function prepare_seeds(seed_json::String,seed_key::String,compounds::Array{String,1})
+    seed_dict = JSON.parsefile(seed_json)
+    [Int(i in seed_dict[seed_key]) for i in compounds]
+end
 
 function netexp(R::Array{Int,2},P::Array{Int,2},x::Array{Int,1})
     
@@ -103,51 +93,85 @@ function netexp(R::Array{Int,2},P::Array{Int,2},x::Array{Int,1})
     end
     (X,Y)
 end
-    
 
+# seed_compounds = JSON.parsefile("../seeds.json");
+# x = [Int(i in seed_compounds["Enceladus_20-SAFR-032"]) for i in compounds];
+
+# ds80_seeds = ["C00011",
+# "C20298",
+# "C14819",
+# "C00087",
+# "C00237",
+# "C00058",
+# "C00033",
+# "C00031",
+# "C00095",
+# "C00124",
+# "C00159",
+# "C00243",
+# "C00208",
+# "C00282",
+# "C00007",
+# "C00001"]
+
+# ds80_seeds = ["C00031","C00001"]
+
+
+
+reaction_edges_json = "kegg/2018-09-25/reaction_edges.json"
+target_json = "../targets/Freilich09.json"
+seed_json = "../seeds.json"
+
+(R,P,compounds,reactions,t) = prepare_matrices_and_targets(reaction_edges_json,target_json)
+x = prepare_seeds(seed_list,compounds)
 (X,Y) = netexp(R,P,x)
 
-size(X)
 
-size(Y)
+function write_netexp_results(x::Array{Int,1},t::Array{Int,1},compounds::Array{String,1},reactions::Array{String,1},X::Array{Any,1},Y::Array{Any,1})
+    data = Dict("stats"=>Dict(),"generations"=>Dict())
 
-X
+    ## Store data about the scope, about the equilibrium
 
-for i in 1:length(X)
-    println(sum(X[i]))
-end
-
-for i in 1:length(Y)
-    println(sum(Y[i]))
-end
-
-# unique([Y[1][i]==1 ? reactions[i] : "SKIP" for (i,r) in enumerate(reactions)])
-
-
-length(reactions)
-
-length(compounds)
-
-reactions_by_generation = Dict()
-for j in 1:size(Y)[1]
-    reactions_by_generation[j] = []
-    for (i,r) in enumerate(reactions)
-        if Y[j][i]==1
-            push!(reactions_by_generation[j],reactions[i])
+    ## Store data by generation
+    n_generations = length(X)-1 ## Last two generations are repeated for compounds
+    for gen in 1:n_generations:
+        
+        data["generations"][gen] = Dict()
+        data["generations"][gen]["reactions_cumulative"] = []
+        data["generations"][gen]["compounds_cumulative"] = []
+        data["generations"][gen]["reactions_new"] = []
+        data["generations"][gen]["compounds_new"] = []
+        
+        ## Add cumulative reaction list for each generation
+        for (i,r) in enumerate(reactions)
+            if Y[gen][i]==1
+                push!(data["generations"][gen]["reactions_cumulative"],reactions[i])
+            end
         end
-    end
-end
 
-compounds_by_generation = Dict()
-for j in 1:size(X)[1]
-    compounds_by_generation[j] = []
-    for (i,c) in enumerate(compounds)
-        if X[j][i]==1
-            push!(compounds_by_generation[j],compounds[i])
+        ## Add cumulative compound list for each generation
+        for (i,c) in enumerate(compounds)
+            if X[gen][i]==1
+                push!(data["generations"][gen]["compounds_cumulative"],compounds[i])
+            end
         end
-    end
-end
 
-reactions_by_generation
+        ## Store new reactions and compounds
+        if gen!=1
+            data["generations"][gen]["reactions_new"] = setdiff(Set(data["generations"][gen]["reactions_cumulative"]) - Set(data["generations"][gen-1]["reactions_cumulative"]))
+            data["generations"][gen]["compounds_new"] = setdiff(Set(data["generations"][gen]["compounds_cumulative"]) - Set(data["generations"][gen-1]["compounds_cumulative"]))
 
-compounds_by_generation
+        ## Store cumulative percent of reactions in scope
+        ## Store cumulative percent of compounds in scope
+
+        ## Store cumulative targets
+        ## Store new targets
+        ## Store cumulative percent of targets
+
+        for i in 1:length(X)
+            println(sum(X[i]))
+        end
+
+        for i in 1:length(Y)
+            println(sum(Y[i]))
+        end
