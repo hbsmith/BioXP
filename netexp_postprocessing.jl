991506d737c5a0f4d20096d77f5ff46967ee5b93
Harrison Smith <hbs@asu.edu>
2018-10-05 20:32:09 -0700

    ran post-processinging on updated bacteria

--- netexp_postprocessing.jl
+++ netexp_postprocessing.jl
@@ -94,7 +94,7 @@ end
 #########################
 ### FORMAT MANY FILES
 #########################
-DATADIR = "results/simple/ph_edge_jsons_P/archaea/"
+DATADIR = "results/simple/ph_edge_jsons/bacteria/"
 
 fsplit = split(DATADIR,"/")
 OUTDIR = "results/formatted/"*fsplit[end-2]*"/"*fsplit[end-1]*"/"
