92b7d4af042e7001ef097ab7bfbf40da19b5cd21
Harrison Smith <hbs@asu.edu>
2018-10-08 11:15:21 -0700

    got rid of generations df

--- netexp_preparescoperandomization.py
+++ netexp_preparescoperandomization.py
@@ -13,8 +13,18 @@ def load_json(fname):
     with open(fname) as f:
         return json.load(f)
 
+def write_json(data,fname):
+    """
+    Wrapper to write json in single line
+
+    :param data: the data to write out
+    :param fname: the filepath to write out
+    """
+    with open(fname, 'w') as outfile:
+        json.dump(data, outfile)
+
 def read_formatted_jsons(INDIR):
-    generation_dfs = []
+    # generation_dfs = []
     stats_dicts = []
     
     for domain in os.listdir(INDIR):
@@ -30,9 +40,9 @@ def read_formatted_jsons(INDIR):
             datajson["stats"]["path"] = fname
 
             stats_dicts.append(datajson["stats"])
-            generation_dfs.append(pd.DataFrame(datajson["generations"]))
+            # generation_dfs.append(pd.DataFrame(datajson["generations"]))
             
-    return generation_dfs, stats_dicts
+    return stats_dicts
 
 def read_molecular_masses(cpd_dir):
     
@@ -87,38 +97,41 @@ def swap_random(tup1,tup2,beta):
     else:
         return False
 
+def main():
 
-n_swaps = 1000
-n_randomizations = 100
-beta = 20
+    n_swaps = 1000
+    n_randomizations = 100
+    beta = 20
 
-cpd_dir = "kegg/2018-09-25/compound/"
-edge_json_dir = "results/formatted/ph_edge_jsons/"
+    cpd_dir = "kegg/2018-09-25/compound/"
+    edge_json_dir = "results/formatted/ph_edge_jsons/"
 
-kegg_masses = read_molecular_masses(cpd_dir)
-generation_dfs, stats_dicts = read_formatted_jsons(edge_json_dir)
-ph_archaea_stats_dicts = [i for i in stats_dicts if i["domain"]=="archaea"]
-# ph_bacteria_stats_dicts = [i for i in stats_dicts if i["domain"]=="bacteria"]
-# archaea_max_scopes = [i["scope_compounds"] for i in ph_archaea_stats_dicts]
-# bacteria_max_scopes = [i["scope_compounds"] for i in ph_bacteria_stats_dicts]
+    kegg_masses = read_molecular_masses(cpd_dir)
+    stats_dicts = read_formatted_jsons(edge_json_dir)
+    ph_archaea_stats_dicts = [i for i in stats_dicts if i["domain"]=="archaea"]
+    # ph_bacteria_stats_dicts = [i for i in stats_dicts if i["domain"]=="bacteria"]
+    # archaea_max_scopes = [i["scope_compounds"] for i in ph_archaea_stats_dicts]
+    # bacteria_max_scopes = [i["scope_compounds"] for i in ph_bacteria_stats_dicts]
 
-org_randomization_dict = dict()
-for org in ph_archaea_stats_dicts:
-    print "Randomizing %s"%org["org_id"]
+    org_randomization_dict = dict()
+    for org in ph_archaea_stats_dicts:
+        print "Randomizing %s"%org["org_id"]
 
-    randomized_cpd_lists = list()
-    for r in range(n_randomizations):
-        print "Randomization %s"%r
-        new_sorted_org_cpd_masses = initialize_randomization_for_organism_scope(org["scope_compounds"],kegg_masses)
-        new_sorted_org_cpd_masses = mix_it_up(new_sorted_org_cpd_masses,beta,n_swaps)
-        randomized_cpd_list = [i[0] for i in new_sorted_org_cpd_masses]
-        
-        if randomized_cpd_list not in randomized_cpd_lists:
-            randomized_cpd_lists.append(randomized_cpd_list)
+        randomized_cpd_lists = list()
+        for r in range(n_randomizations):
+            print "Randomization %s"%r
+            new_sorted_org_cpd_masses = initialize_randomization_for_organism_scope(org["scope_compounds"],kegg_masses)
+            new_sorted_org_cpd_masses = mix_it_up(new_sorted_org_cpd_masses,beta,n_swaps)
+            randomized_cpd_list = [i[0] for i in new_sorted_org_cpd_masses]
+            
+            if randomized_cpd_list not in randomized_cpd_lists:
+                randomized_cpd_lists.append(randomized_cpd_list)
 
-    org_randomization_dict[org["org_id"]] = randomized_cpd_lists
+        org_randomization_dict[org["org_id"]] = randomized_cpd_lists
 
-for org in org_randomization_dict:
-    print org, len(org_randomization_dict[org])
+    for org in org_randomization_dict:
+        write_json(org_randomization_dict[org],org)
+        # print org, len(org_randomization_dict[org])
 
-
+if __name__ == '__main__':
+    main()
