34135ab8b9deabdf5f3a9ac690068702ff0957a0
Harrison Smith <hbs@asu.edu>
2018-10-08 01:00:30 -0700

    ready to test it

--- netexp_preparescoperandomization.py
+++ netexp_preparescoperandomization.py
@@ -6,6 +6,7 @@ import os
 import random
 import collections
 import copy
+import math
 
 def load_json(fname):
     with open(fname) as f:
@@ -41,23 +42,76 @@ def read_molecular_masses(cpd_dir):
 
     return kegg_masses ## Tuple of (cpd_id, mass)
 
-org_cpd_masses = list()
-for cpd in archaea_max_scopes[0]:
-    org_cpd_masses.append((cpd,kegg_masses[cpd]))
+def initialize_randomization_for_organism_scope(org_max_scope,kegg_masses):
+        
+    org_cpd_masses = list()
+    for cpd in org_max_scope:
+        org_cpd_masses.append((cpd,kegg_masses[cpd]))
 
-sorted_org_cpd_masses = sorted([i for i in org_cpd_masses if i[1]!=0], key=lambda x: x[1], reverse=True)
-org_cpd_zeros = [i for i in org_cpd_masses if i[1]==0]
+    sorted_org_cpd_masses = sorted([i for i in org_cpd_masses if i[1]!=0], key=lambda x: x[1], reverse=True)
+    org_cpd_zeros = [i for i in org_cpd_masses if i[1]==0]
 
-new_sorted_org_cpd_masses = copy.deepcopy(sorted_org_cpd_masses)
-for i,tup in enumerate(org_cpd_zeros):
-    new_sorted_org_cpd_masses.insert(random.randrange(len(sorted_org_cpd_masses)+i), tup)
+    new_sorted_org_cpd_masses = copy.deepcopy(sorted_org_cpd_masses)
+    for i,tup in enumerate(org_cpd_zeros):
+        new_sorted_org_cpd_masses.insert(random.randrange(len(sorted_org_cpd_masses)+i), tup)
+    
+    return new_sorted_org_cpd_masses
 
+def mix_it_up(tup_list,beta,n):
+    for _ in range(n):
+        i1, i2 = random.sample(range(len(tup_list)),2)  ## 2 random indices
+        if swap_random(tup_list[i1],tup_list[i2],beta) == True:
+            tup_list[i1], tup_list[i2] = tup_list[i2], tup_list[i1] 
+        else: 
+            pass
+    return tup_list
+
+def swap_random(tup1,tup2,beta):
+
+    diff = tup1[1]-tup2[1]
+
+    ## Determine probability of flipping
+    if tup1[1]==0 or tup2[1]==0:
+        p = .5 # flip with 50% chance
+    
+    elif diff > 0:
+        p = math.e**(-diff/float(beta))
+
+    elif diff <= 0:
+        p = 1.0
+
+    ## Flip and return
+    if random.random()<p:
+        return True
+    else:
+        return False
+
+
+n_swaps = 10000
+n_randomizations = 1000
+beta = 20
 
 cpd_dir = "kegg/2018-09-25/compound/"
 edge_json_dir = "results/formatted/ph_edge_jsons/"
 
+kegg_masses = read_molecular_masses(cpd_dir)
 generation_dfs, stats_dicts = read_formatted_jsons(edge_json_dir)
 ph_archaea_stats_dicts = [i for i in stats_dicts if i["domain"]=="archaea"]
-ph_bacteria_stats_dicts = [i for i in stats_dicts if i["domain"]=="bacteria"]
+# ph_bacteria_stats_dicts = [i for i in stats_dicts if i["domain"]=="bacteria"]
+# archaea_max_scopes = [i["scope_compounds"] for i in ph_archaea_stats_dicts]
+# bacteria_max_scopes = [i["scope_compounds"] for i in ph_bacteria_stats_dicts]
 
-kegg_masses = read_molecular_masses(cpd_dir)
+for org in ph_archaea_stats_dicts:
+
+    randomized_cpd_lists = list()
+    for _ in range(n_randomizations):
+        new_sorted_org_cpd_masses = initialize_randomization_for_organism_scope(org["scope_compounds"],kegg_masses)
+        new_sorted_org_cpd_masses = mix_it_up(new_sorted_org_cpd_masses,beta,n_swaps)
+        randomized_cpd_list = [i[0] for i in new_sorted_org_cpd_masses]
+        
+        if randomized_cpd_list not in randomized_cpd_lists:
+            randomized_cpd_lists.append(randomized_cpd_list)
+
+    ph_archaea_stats_dicts["randomized_cpd_lists"] = randomized_cpd_lists
+
+
