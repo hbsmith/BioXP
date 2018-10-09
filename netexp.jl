397c0a61b8aeee4694890800f3a9ac1e74392c2b
Harrison Smith <hbs@asu.edu>
2018-10-09 09:49:57 -0700

    ran netexp for more archaea and bacteria

--- netexp.jl
+++ netexp.jl
@@ -198,9 +198,9 @@ end
 
 # SEEDJSON = "29012812801.json" ## Contains keys numbered 1-100, with values of random compounds
 TARGETJSON = "targets/Freilich09.json"
-EDGEDIR = "jgi/2018-09-29/ph_edge_jsons/archaea/"
-SEEDDIR = "seeds/minimal_seed_randomizations/archaea/"
-OUTDIR = "results/simple/minimal_seed_randomizations_fixed/archaea/"  #*split(SEEDJSON,".json")[1]*"/"
+EDGEDIR = "jgi/2018-09-29/ph_edge_jsons/bacteria_split/b10/"
+SEEDDIR = "seeds/minimal_seed_randomizations/bacteria/"
+OUTDIR = "results/simple/minimal_seed_randomizations_fixed/bacteria/"  #*split(SEEDJSON,".json")[1]*"/"
 
 if ispath(OUTDIR)==false
     mkpath(OUTDIR)
