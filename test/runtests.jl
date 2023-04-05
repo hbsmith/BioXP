71b49cfb36b54c2641325eb05f4b286823c05daf
hbsmith <hbs@asu.edu>
2023-04-05 13:41:52 +0900

    add back tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,8 +2,8 @@ using Test
 using BioXP
 using DelimitedFiles
 
-# include("parser.jl")
+include("parser.jl")
 # include("randomize.jl")
-# include("BioXP.jl")
-# include("dg.jl")
+include("BioXP.jl")
+include("dg.jl")
 include("ne_comparison.jl")
